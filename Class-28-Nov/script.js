9a9f53be1d9a34a0a2d5ae2ea537af24ce289c3e
Huzaifa Quadri <huzaifaquadri1853@gmail.com>
2025-12-08 10:21:09 +0530

    Advanced Css

--- Class-28-Nov/script.js
+++ Class-28-Nov/script.js
@@ -1,39 +1,38 @@
-let p = document.querySelector("p");
-const h1 = document.querySelector("h1");
-let text = p.innerText;
-console.log(text);
+const elements = document.querySelectorAll(".matrix-effect");
 const Characters =
   "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
 
-// let iteration = 0;
-let itr = 0;
-function randomText() {
-  const str = text
-    .split("")
-    .map((char, ind) => {
-      if (ind < itr) {
-        return char;
-      }
-      return Characters.split("")[
-        Math.floor(Math.random() * Characters.length)
-      ];
-    })
-    .join("");
+elements.forEach((element) => {
+  let text = element.innerText;
+  let itr = 0;
+  let interval; //just for clear interval
 
-  itr += 0.25;
-  // console.log(itr);
-  p.innerText = str;
-}
-let interval;
+  function randomText() {
+    const str = text
+      .split("")
+      .map((char, ind) => {
+        if (ind < itr) {
+          return char;
+        }
+        return Characters.split("")[
+          Math.floor(Math.random() * Characters.length)
+        ];
+      })
+      .join("");
 
-p.addEventListener("mouseenter", () => {
-  itr = 0;
-  interval = setInterval(() => {
-    randomText();
-  }, 30);
-});
+    itr += 0.25;
+    element.innerText = str;
+  }
 
-p.addEventListener("mouseleave", () => {
-  clearInterval(interval);
-  p.innerText = text;
+  element.addEventListener("mouseenter", () => {
+    itr = 0;
+    interval = setInterval(() => {
+      randomText();
+    }, 30);
+  });
+
+  element.addEventListener("mouseleave", () => {
+    clearInterval(interval);
+    element.innerText = text;
+  });
 });
